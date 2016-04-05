481a8c454bf9f143031117f959ddb6af6f92b85b
Jackie Mundrawala <jmundrawala@gmail.com>
2016-04-05 13:41:02 +0000

    Ability to add new gram to database

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,10 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
+if ActiveRecord::Migrator.needs_migration?
+  ActiveRecord::Migrator.migrate(File.join(Rails.root, 'db/migrate'))
+end
+
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are

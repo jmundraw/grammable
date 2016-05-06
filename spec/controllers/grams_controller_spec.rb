1d43ecb79827db05352f60c9d2a30ff04b489c58
Jackie Mundrawala <jmundrawala@gmail.com>
2016-05-06 02:28:47 +0000

    connect user and gram models

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe GramsController, type: :controller do
+  describe "grams#show action" do 
+    it "should successfully show the page if gram is found" do
+      # gram = FactoryGirl.create(:gram)
+      gram = Gram.create(message: "hello")
+      get :show, id: gram.id
+      expect(response).to have_http_status(:success)
+    end
+
+    it "should return a 404 error if the gram is not found" do 
+      get :show, id: 'Hello'
+      expect(response).to have_http_status(:not_found)
+    end
+  end 
+
   describe "grams#index action" do 
   	it "should successfully show the page" do 
   		get :index
@@ -10,25 +24,58 @@ RSpec.describe GramsController, type: :controller do
 
 
   describe "grams#new action" do 
-  	it "should successfully show the new form" do 
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
+  	it "should successfully show the new form" do
+      # user = FactoryGirl.create(:user)
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )      
+      sign_in user
   		get :new
   		expect(response).to have_http_status(:success)
   	end
   end
 
   describe "grams#create action" do 
+    it "should require users to be logged in" do
+      post :create, gram: { message: "Hello"}
+      expect(response).to redirect_to new_user_session_path
+
+    end
+
     it "should not allow blank messages to be saved" do
+      user = User.create(
+        email:                 'fakeuser@gmail.com',
+        password:              'secretPassword',
+        password_confirmation: 'secretPassword'
+      )
+      # FactoryGirl.create(:user)
+      sign_in user
       post :create, gram: {message: ""}
       expect(response).to have_http_status(:unprocessable_entity)
       expect(Gram.count).to eq 0
     end
 
-    it "should successfully create a new gram in our database" do 
+    it "should successfully create a new gram in our database" do
+      # user = FactoryGirl.create(:user)
+        user = User.create(
+          email:                 'fakeuser@gmail.com',
+          password:              'secretPassword',
+          password_confirmation: 'secretPassword'
+        )
+      sign_in user 
       post :create, gram: {message: "Hello!"}
       expect(response).to redirect_to root_path
       
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
   end
 end
---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+# FactoryGirl.define do
+#   factory :user do
+#     sequence :email do |n|
+#       "dummyEmail#{n}@gmail.com"
+#     end
+#     password "password"
+#     password "password"
+#   end
+
+    # facotry :gram do
+    #   message "hello"
+    #   association :user
+    # end
+# end
